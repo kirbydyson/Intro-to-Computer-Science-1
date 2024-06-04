a308f2e1ab71bbf3cffda599f673026571d697ca
kirbydyson <117674986+kirbydyson@users.noreply.github.com>
2024-06-04 17:26:11 -0500

    Add files via upload

---
+++ Program 2/Program2.cpp
@@ -0,0 +1,24 @@
+/*
+*Author: Kirby Dyson
+*Assignment Title: Print a Table 2
+*Assignment Description: This program prints a table.
+*Due Date: 8/26/2022
+*Date Created: 8/24/2022
+*Date Last Modified: 8/25/2022
+*/
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "A    B    A^B" << endl;
+    cout << "1    0    1" << endl;
+    cout << "2    1    2" <<endl;
+    cout << "3    2    9" << endl;
+    cout << "4    3    64" << endl;
+    cout << "5    4    625" << endl;
+
+    return 0;
+}
