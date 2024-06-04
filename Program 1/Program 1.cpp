933363e89ac29eabe92dab17d19595d5455d9c87
kirbydyson <117674986+kirbydyson@users.noreply.github.com>
2024-06-04 17:26:00 -0500

    Add files via upload

---
+++ Program 1/Program 1.cpp
@@ -0,0 +1,20 @@
+/*
+*Author: Kirby Dyson
+*Assignment Title: Hello World
+*Assignment Description: This program prints the text Hello World.
+*Due Date: 8/24/2022
+*Date Created: 8/23/2022
+*Date Last Modified: 8/24/2022
+*/
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "Hello World!:-)" << endl;
+
+    return 0;
+}
+
